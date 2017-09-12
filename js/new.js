094114629acd795083c70320b17b7247cc62f5bc
Alan Boulton <alan.boulton@gmail.com>
2017-09-12 23:36:44 +0100

    Updated new.js

--- js/new.js
+++ js/new.js
@@ -1,3 +1,14 @@
-(function () {
+'use strict'
 
-})();
+function Hinter(input_id, output_id){
+    this.input_id = input_id;
+    this.output_id = output_id;
+}
+
+Hinter.prototype.lookup = function(command_string){
+    if (command_string.equals('ls')) {
+        return 'ls';
+    } else {
+        return "";
+    }
+};
